33348f2602f43ca11dfe4008d5d23bd4caff04ef
seajoyer <imgarison@gmail.com>
2025-05-05 03:19:51 +0300

    Reorganize

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Jupyter Notebook with ML libraries";
+  description = "Jupyter Notebook with ML libraries and Matplotlib Dracula theme";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -9,56 +9,110 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pythonEnv = pkgs.python3.withPackages (ps:
-          with ps; [
-            jupyter
-            numpy
-            pandas
-            matplotlib
+        # Allow unfree packages
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
 
-            (buildPythonPackage rec {
-              pname = "jupyterlab_vim";
-              version = "4.1.3";
+        # Python packages
+        jupyterlabVim = pkgs.python3.pkgs.buildPythonPackage rec {
+          pname = "jupyterlab_vim";
+          version = "4.1.3";
+          format = "pyproject";
 
-              src = fetchPypi {
-                inherit pname version;
-                sha256 = "sha256-V+GgpO3dIzTo16fA34D1CXt49UgP+oQwfy5QjfmLaHg=";
-              };
+          src = pkgs.python3.pkgs.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-V+GgpO3dIzTo16fA34D1CXt49UgP+oQwfy5QjfmLaHg=";
+          };
 
-              doCheck = false;
-              format = "pyproject";
+          propagatedBuildInputs = with pkgs.python3.pkgs; [
+            hatchling
+            jupyterlab
+            hatch-nodejs-version
+            hatch-jupyter-builder
+          ];
 
-              propagatedBuildInputs = [
-                hatchling
-                jupyterlab
-                hatch-nodejs-version
-                hatch-jupyter-builder
-              ];
-            })
-          ]);
+          doCheck = false;
+        };
 
+        # Python environment with all packages
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          # Core data science
+          jupyter
+          numpy
+          pandas
+
+          # Visualization
+          matplotlib
+          seaborn
+
+          # Machine learning
+          scikit-learn
+          kaggle
+
+          # Deep learning (unfree packages)
+          torch-bin
+          torchvision-bin
+
+          # Jupyter extensions
+          jupyterlabVim
+        ]);
+
+        # Script to set up Dracula theme for Matplotlib
+        draculaThemeSetup = pkgs.writeShellScript "setup-dracula-theme" ''
+          MATPLOTLIB_CONFIG_DIR="$HOME/.config/matplotlib"
+          mkdir -p "$MATPLOTLIB_CONFIG_DIR"
+
+          if [ ! -f "$MATPLOTLIB_CONFIG_DIR/matplotlibrc" ]; then
+            ${pkgs.curl}/bin/curl -L "https://raw.githubusercontent.com/dracula/matplotlib/master/dracula.mplstyle" \
+              -o "$MATPLOTLIB_CONFIG_DIR/dracula.mplstyle"
+            echo "backend: TkAgg" > "$MATPLOTLIB_CONFIG_DIR/matplotlibrc"
+            echo "style: dracula.mplstyle" >> "$MATPLOTLIB_CONFIG_DIR/matplotlibrc"
+          fi
+        '';
+
+        # Jupyter wrapper that sets up config and theme
         jupyterWrapper = pkgs.writeShellScriptBin "jupyter-notebook" ''
+          # Set up Jupyter config directory
           export JUPYTER_CONFIG_DIR="$PWD/.jupyter"
           if [ ! -d "$JUPYTER_CONFIG_DIR" ]; then
             mkdir -p "$JUPYTER_CONFIG_DIR"
             echo "c.NotebookApp.notebook_dir = '$PWD'" > "$JUPYTER_CONFIG_DIR/jupyter_notebook_config.py"
           fi
+
+          # Set up Dracula theme
+          ${draculaThemeSetup}
+
+          # Launch Jupyter
           ${pythonEnv}/bin/jupyter-notebook
         '';
       in {
+        # Default package is just the Jupyter wrapper
         packages.default = jupyterWrapper;
 
+        # Default app is the Jupyter wrapper
         apps.default = {
           type = "app";
           program = "${jupyterWrapper}/bin/jupyter-notebook";
         };
 
-        devShells.default =
-          pkgs.mkShell { buildInputs = [ pythonEnv jupyterWrapper pkgs.git ]; };
+        # Development shell with all tools
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            jupyterWrapper
+            pkgs.git
+          ];
+
           shellHook = ''
             echo "Git version: $(git --version)"
             echo "Jupyter environment is ready. Run 'jupyter-notebook' to start."
+            echo "Matplotlib Dracula theme will be set up automatically when you run jupyter-notebook."
           '';
-      });
+        };
+      }
+    );
 }
